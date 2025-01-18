2c4767ad1256fb30dcebc91e47543020277d2f75
M.F.Aziz <azizsagi@live.com>
2025-01-18 16:16:31 +0000

    Added my module with arithmetic functions.

---
+++ main.py
@@ -0,0 +1,38 @@
+
+# Python BUild in functions 
+
+# print("Welcome Mahad")
+# print("welcome Kim")
+# print("Welcome Ali")   
+
+# len()
+# range()
+# type()
+# .upper()
+# .lower()
+
+# Function is a block of code. that we can reuse.
+
+# def name_of_the_function():
+#     defonition of the function
+
+
+# Define a function = default values to the parameters Required and Optional
+# def greet_me(firstName, lastName, age="not applicable", city="Unknown"):
+#     print("Welcome to Skill City ",firstName, lastName)
+#     print("Your age is ", age)
+#     print("You are from ", city)
+
+
+
+
+# # Call a function
+# greet_me("Mahad")
+
+import mymodule as mm # custom module / user defined module
+
+
+print(mm.sum(2,3)) # 5
+print(mm.sub(2,3)) # -1
+print(mm.mul(2,3)) # 6
+print(mm.div(2,3)) # 0.6666666666666666
---
+++ mymodule.py
@@ -0,0 +1,17 @@
+# Arithmentic Functions - functional programming - General Purpose Programming
+def sum(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+def div(a,b):
+    return a / b
+
+# Variables
+pi = 3.14
+username = "admin"
+password = "admin123"
